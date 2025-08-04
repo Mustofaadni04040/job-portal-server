f33e686fd275baf8fb9bff8aca38cdf431bcea80
mustofa adny <adniadni04@gmail.com>
2025-08-05 00:39:21 +0700

    update: method route logout

--- routes/user.route.js
+++ routes/user.route.js
@@ -13,7 +13,7 @@ const router = express.Router();
 
 router.route("/register").post(singleUpload, register);
 router.route("/login").post(login);
-router.route("/logout").get(logout);
+router.route("/logout").post(logout);
 router
   .route("/profile/update")
   .put(isAuthenticated, singleUpload, updateProfile);
