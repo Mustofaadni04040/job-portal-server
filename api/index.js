09bbc58ff7862087b64ca8098b2e5b1b21f140cd
mustofa adny <adniadni04@gmail.com>
2025-08-04 18:04:48 +0700

    update: vercel.json

--- api/index.js
+++ api/index.js
@@ -2,12 +2,12 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.route.js";
-import companyRoute from "./routes/company.route.js";
-import jobRoute from "./routes/job.route.js";
-import applicationRoute from "./routes/application.route.js";
-import bookmarkRoute from "./routes/bookmark.route.js";
+import connectDB from "../utils/db.js";
+import userRoute from "../routes/user.route.js";
+import companyRoute from "../routes/company.route.js";
+import jobRoute from "../routes/job.route.js";
+import applicationRoute from "../routes/application.route.js";
+import bookmarkRoute from "../routes/bookmark.route.js";
 
 dotenv.config({});
 
